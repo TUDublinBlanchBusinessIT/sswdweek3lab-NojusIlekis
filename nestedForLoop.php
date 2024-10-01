110444addb69b8678abb031878c7ef6475bdb2c6
NojusIlekis <b00100794@mytudublin.ie>
2024-10-01 15:55:17 +0100

    found out on w3 school how to edit cell padding and table border

---
+++ nestedForLoop.php
@@ -0,0 +1,17 @@
+<?php
+// nestedForLoop.php
+
+echo "<table border='1' cellpadding='5'>";
+
+for ($i = 1; $i <= 15; $i++) {
+    echo "<tr>";
+    
+    for ($j = 1; $j <= 20; $j++) {
+        echo "<td>$i,$j</td>";
+    }
+    
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
